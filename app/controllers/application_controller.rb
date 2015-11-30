e4900383bb3643c9cc74d59b9ab6ce0f3c974bd7
Steven Cassidy <steven@mumsnet.com>
2015-11-30 11:26:18 -0500

    Add an ensure_user before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  before_action :ensure_current_user, only: [:show, :create, :update, :destroy]
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -11,6 +12,12 @@ class ApplicationController < ActionController::Base
 
   def is_logged_in?
     !!current_user
+  end
+
+  private 
+  
+  def ensure_current_user
+    redirect_to root_path unless current_user
   end
 
 end
