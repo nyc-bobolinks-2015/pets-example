9b5ca93e9c8311a1fb3458d86c796edd1648fb4d
Steven Cassidy <steven@mumsnet.com>
2015-11-30 10:48:35 -0500

    Add validations to dog

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,13 @@
 class Dog < ActiveRecord::Base
+  validates :breed, presence: true
+  validate :dog_is_young
+  
+  private
+  
+  def dog_is_young
+    if date_of_birth < Date.parse('2015-01-01')
+      errors.add(:date_of_birth, "Must be after Jan 1 2015")
+      errors.add(:base, 'Some random message')
+    end
+  end
 end
