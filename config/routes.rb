77376c29e4f94a091cfd1983de9ecfb84b9ed5f6
Steven Cassidy <steven@mumsnet.com>
2015-11-30 14:40:16 -0500

    Change root to cats#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resource :session, only: [:create, :destroy]
   get '/login' => 'sessions#new'
   get '/logout' => 'sessions#destroy'
-  root 'welcome#index'
+  root 'cats#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
