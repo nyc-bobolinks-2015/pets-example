0290bb9d61e1bb871a38603cf18cac8a52f9154e
Steven Cassidy <steven@mumsnet.com>
2015-11-30 16:39:48 -0500

    Implement create / update / edit cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,35 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    @cat.name = params[:cat][:name]
+
+    @cat.assign_attributes(permitted_params)
+    if @cat.save
+      flash[:notice] = 'Saved the cat'
+      redirect_to cat_path(@cat)
+    else
+      flash.now[:error] = 'Failed to save cat'
+      render :edit
+    end
+
+  end
+
+  private
+
+  def permitted_params
+    params.require(:cat).permit(:name, :breed, :age, :nickname)
+  end
+
+
+
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,5 +1,7 @@
 class Cat < ActiveRecord::Base
 
+  validates :nickname, presence: true
+
   def to_param
     "#{id}-#{name.parameterize}"
   end
