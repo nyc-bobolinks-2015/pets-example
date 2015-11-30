248e21a987a37ab5afb586dc7faa98e10ca13976
Steven Cassidy <steven@mumsnet.com>
2015-11-30 14:31:21 -0500

    Implement a login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   skip_before_action :ensure_current_user
+
+
+  def create
+    u = User.find_by(username: params[:username])
+    if u && u.authenticate(params[:password])
+      session[:user_id] = u.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
   
 end
